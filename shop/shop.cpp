c89ff7faa156b8228a0ec93498a04c210a077c1c
VPMateev18 <vpmateev18@codingburgas.bg>
2020-04-18 18:56:15 +0100

    comparison function
    this function checks two clothes for our requests

--- shop/shop.cpp
+++ shop/shop.cpp
@@ -29,6 +29,17 @@ bool ComparePrice(CLOTHES clothe1, CLOTHES clothe2)
     return clothe1.price < clothe2.price;
 }
 
+bool CompareForVolen(CLOTHES clothe1, CLOTHES clothe2)
+{
+    if (clothe1.type.gender != 'F' && clothe2.type.gender == 'F') return true;
+    else if (clothe1.type.gender == 'F' && clothe2.type.gender != 'F') return false;
+
+    if (clothe1.type.sz == "M" && clothe2.type.sz != "M") return true;
+    else if (clothe1.type.sz != "M" && clothe2.type.sz == "M") return false;
+   
+   return clothe1.price<clothe2.price;
+}
+
 void sortByPrice(CLOTHES* shop, int clothesCount) ///sorts the clothes by price from lower to higher
 {
     sort(shop, shop + clothesCount, ComparePrice);
@@ -50,30 +61,30 @@ CLOTHES getClothesById(CLOTHES* shop, int clothesCount, int id)  ///returns clot
 int main()
 {
 
-    ///DATA
-
-    CLOTHES shop[200] = {
+  ///DATA
+   CLOTHES shop[200] = {
         {"red", "shirt", "S", "hm", 'M', 100, 1, 55},
         {"blue", "sweatshirt", "M", "gucci", 'W', 100, 2, 40},
         {"red", "t-shirt", "M", "armani", 'B', 100, 3, 150},
         {"brown", "sweater", "L", "armani", 'B', 100, 4, 50},
         {"red", "sweatpants", "M", "armani", 'W', 100, 5, 75},
-        {"red", "shorts", "L", "armani", 'M', 100, 6, 70},
+        {"yellow", "shorts", "L", "armani", 'M', 100, 6, 70},
     };
 
-    ///DATA
+   ///DATA
 
-    ///TESTING THE FUNCTIONS
+   ///TESTING THE FUNCTIONS
 
-    int clothesCount = 6;
-    sortByPrice(shop, clothesCount);
-    cout << shop[0].price << endl;
-    CLOTHES a;
-    a = getClothesById(shop, clothesCount, 5);
-    cout << a.id << endl;
+   int clothesCount = 6;
+   /**sortByPrice(shop, clothesCount);
+   cout << shop[0].price << endl;
+   CLOTHES a;
+   a = getClothesById(shop, clothesCount, 5);
+   cout << a.id << endl;     */       
+  if(CompareForVolen( shop[2], shop[3])==true) cout<<shop[2].type.model <<"AAAA"<<endl;
+  else cout<<shop[3].type.model<<"BBB"<<endl;
 
 
-
-    ///TESTING THE FUNCTIONS
-    return 0;
+   ///TESTING THE FUNCTIONS
+  return 0;
 }
